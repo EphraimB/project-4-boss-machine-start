f03383739f02e28a22f5f9026e21f74990fee4cc
Ephraim Becker <emb16@outlook.com>
2022-11-11 13:16:41 -0500

    Got the front end to show up.

--- server/api.js
+++ server/api.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const app = require('../server');
 const apiRouter = express.Router();
 
+const { 
+    addToDatabase,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+  } = require('./db');
 
+app.use('/api', apiRouter);
+
+apiRouter.get('/minions', (req, res, next) => {
+    res.send(getAllFromDatabase('minions'));
+})
 
 module.exports = apiRouter;
