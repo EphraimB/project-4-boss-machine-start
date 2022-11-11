2122ec82ddbb1d8efdc72833dc1462d0621c9297
Ephraim Becker <emb16@outlook.com>
2022-11-11 13:40:53 -0500

    Trying to get minionId to work.

--- server/api.js
+++ server/api.js
@@ -16,4 +16,12 @@ apiRouter.get('/minions', (req, res, next) => {
     res.send(getAllFromDatabase('minions'));
 })
 
+apiRouter.post('/minions', (req, res, next) => {
+    res.send(addToDatabase('minions'));
+})
+
+apiRouter.get('/minions:minionId', (req, res, next) => {
+    res.send(getFromDatabaseById('minions', req.params.minionId));
+})
+
 module.exports = apiRouter;
