bab6f63cb6abad6c8acd557464e92c4b98960518
Ephraim Becker <emb16@outlook.com>
2022-11-13 13:01:42 -0500

    Basically finished the project.

--- server/meetings.js
+++ server/meetings.js
@@ -2,20 +2,28 @@ const express = require('express');
 const db = require('./db');
 const apiRouter = express.Router();
 
+//populate data set
+let meetings = db.getAllFromDatabase('meetings');
+
 apiRouter.get('/', (req, res, next) => {
-    res.send(db.getAllFromDatabase('meetings'));
+    res.send(meetings);
 })
 
 apiRouter.post('/', (req, res, next) => {
-    res.send(db.addToDatabase('meetings'));
+    const newMeeting = db.createMeeting();
+
+    if(!newMeeting) {
+        res.status(400).send();
+    } else {
+        db.addToDatabase('meetings', newMeeting);
+        res.status(201).send(newMeeting);
+    }
 })
 
 apiRouter.delete('/', (req, res, next) => {
-    if(db.deleteAllFromDatabase('meetings')) {
-        res.send(db.deleteAllFromDatabase('meetings'));
-    } else {
-        res.status(404).send();
-    }
+    db.deleteAllFromDatabase('meetings');
+    meetings = [];
+    res.status(204).send();
 });
 
 module.exports = apiRouter;
