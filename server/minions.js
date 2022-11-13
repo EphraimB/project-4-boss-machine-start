2b153a6708f38b66ad1e96e482467482aa40d848
Ephraim Becker <emb16@outlook.com>
2022-11-13 12:49:58 -0500

    Simplified the minions router.

--- server/minions.js
+++ server/minions.js
@@ -2,36 +2,48 @@ const express = require('express');
 const db = require('./db');
 const apiRouter = express.Router();
 
+//populate data set
+let minions = db.getAllFromDatabase('minions');
+
+apiRouter.use('/:minionId', (req, res, next) => {
+    let minionId = req.params.minionId;
+    let minionIndex = minions.findIndex(obj => obj.id === minionId);
+    if(minionIndex !== -1){
+      req.minionIndex = minionIndex;
+      req.minionId = minionId;
+      next();
+    } else {
+      res.status(404).send('Minon not found!');
+    }
+})
+
 apiRouter.get('/', (req, res, next) => {
-    res.send(db.getAllFromDatabase('minions'));
+    res.send(minions);
 })
 
 apiRouter.post('/', (req, res, next) => {
-    res.send(db.addToDatabase('minions'));
+    const receivedMinion = req.body;
+
+    if(!receivedMinion) {
+        res.status(400).send();
+    } else {
+        db.addToDatabase('minions', receivedMinion);
+        res.status(201).send(receivedMinion);
+    }
 })
 
 apiRouter.get('/:minionId', (req, res, next) => {
-    if(db.getFromDatabaseById('minions', req.params.minionId)) {
-        res.send(db.getFromDatabaseById('minions', req.params.minionId));
-    } else {
-        res.status(404).send();
-    }
+    res.send(minions[req.minionIndex]);
 })
 
 apiRouter.put('/:minionId', (req, res, next) => {
-    if(db.updateInstanceInDatabase('minions', req.params.minionId)) {
-        res.send(db.updateInstanceInDatabase('minions', req.params.minionId));
-    } else {
-        res.status(404).send();
-    }
+    db.updateInstanceInDatabase('minions', req.params.minionId);
+    res.send(db.getAllFromDatabase('minions')[req.minionIndex])
 });
 
 apiRouter.delete('/:minionId', (req, res, next) => {
-    if(db.deleteFromDatabasebyId('minions', req.params.minionId)) {
-        res.send(db.deleteFromDatabasebyId('minions', req.params.minionId));
-    } else {
-        res.status(404).send();
-    }
+    db.deleteFromDatabasebyId('minions', req.params.minionId);
+    res.status(204).send();  
 });
 
 module.exports = apiRouter;
