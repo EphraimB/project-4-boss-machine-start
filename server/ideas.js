0a07dda63aaa3f76669955556328634fd6a005e2
Ephraim Becker <emb16@outlook.com>
2022-11-13 12:57:36 -0500

    Simplified ideas route.

--- server/ideas.js
+++ server/ideas.js
@@ -2,36 +2,48 @@ const express = require('express');
 const db = require('./db');
 const apiRouter = express.Router();
 
+//populate data set
+let ideas = db.getAllFromDatabase('ideas');
+
+apiRouter.use('/:ideaId', (req, res, next) => {
+    let ideaId = req.params.ideaId;
+    let ideaIndex = ideas.findIndex(obj => obj.id === ideaId);
+    if(ideaIndex !== -1){
+      req.ideaIndex = ideaIndex;
+      req.ideaId = ideaId;
+      next();
+    } else {
+      res.status(404).send('Idea not found!');
+    }
+})
+
 apiRouter.get('/', (req, res, next) => {
-    res.send(db.getAllFromDatabase('ideas'));
+    res.send(ideas);
 })
 
 apiRouter.post('/', (req, res, next) => {
-    res.send(db.addToDatabase('ideas'));
+    const receivedIdea = req.body;
+
+    if(!receivedIdea) {
+        res.status(400).send();
+    } else {
+        db.addToDatabase('ideas', receivedIdea);
+        res.status(201).send(receivedIdea);
+    }
 })
 
 apiRouter.get('/:ideaId', (req, res, next) => {
-    if(db.getFromDatabaseById('ideas', req.params.ideaId)) {
-        res.send(db.getFromDatabaseById('ideas', req.params.ideaId));
-    } else {
-        res.status(404).send();
-    }
+    res.send(ideas[req.ideaIndex]);
 })
 
 apiRouter.put('/:ideaId', (req, res, next) => {
-    if(db.updateInstanceInDatabase('ideas', req.params.ideaId)) {
-        res.send(db.updateInstanceInDatabase('ideas', req.params.ideaId));
-    } else {
-        res.status(404).send();
-    }
+    db.updateInstanceInDatabase('ideas', req.params.ideaId);
+    res.send(db.getAllFromDatabase('ideas')[req.ideaIndex])
 });
 
 apiRouter.delete('/:ideaId', (req, res, next) => {
-    if(db.deleteFromDatabasebyId('ideas', req.params.ideaId)) {
-        res.send(db.deleteFromDatabasebyId('ideas', req.params.ideaId));
-    } else {
-        res.status(404).send();
-    }
+    db.deleteFromDatabasebyId('ideas', req.params.ideaId);
+    res.status(204).send();  
 });
 
 module.exports = apiRouter;
